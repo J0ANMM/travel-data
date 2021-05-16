c2b7e39b55978a92718c255c90afd2f58fd8ba4d
J0ANMM <joanmm@gmail.com>
2021-05-16 17:06:26 +0200

    Create countries.py

---
+++ countries.py
@@ -0,0 +1,57 @@
+# List of European countries
+
+        #country iso codes used: https://en.wikipedia.org/wiki/ISO_3166-2
+        #country iso ids: https://en.wikipedia.org/wiki/ISO_3166-1_numeric
+
+        #list of timezones: https://stackoverflow.com/questions/13866926/python-pytz-list-of-timezones
+
+        countries = [
+            {"iso_code": "DE", "name": "Germany", "oficial_lan": "German", "tz": "Europe/Berlin"},
+            {"iso_code": "AT", "name": "Austria", "oficial_lan": "German", "tz": "Europe/Vienna"},
+            {"iso_code": "CH", "name": "Switzerland", "oficial_lan": None, "tz": "Europe/Zurich"},
+            {"iso_code": "AL", "name": "Albania", "oficial_lan": None, "tz": "Europe/Tirane"},
+            {"iso_code": "BE", "name": "Belgium", "oficial_lan": None, "tz": "Europe/Brussels"},
+            {"iso_code": "BA", "name": "Bosnia and Herzegovina", "oficial_lan": None, "tz": "Europe/Sarajevo"},
+            {"iso_code": "BG", "name": "Bulgaria", "oficial_lan": None, "tz": "Europe/Sofia"},
+            {"iso_code": "HR", "name": "Croatia", "oficial_lan": None, "tz": "Europe/Zagreb"},
+            {"iso_code": "CY", "name": "Cyprus", "oficial_lan": None, "tz": "Europe/Nicosia"},
+            {"iso_code": "CZ", "name": "Czech Republic", "oficial_lan": None, "tz": "Europe/Prague"},
+            {"iso_code": "DK", "name": "Denmark", "oficial_lan": "Danish", "tz": "Europe/Copenhagen"},
+            {"iso_code": "EE", "name": "Estonia", "oficial_lan": None, "tz": "Europe/Tallinn"},
+            {"iso_code": "FO", "name": "Faroe Islands", "oficial_lan": None, "tz": "Atlantic/Faroe"},
+            {"iso_code": "FI", "name": "Finland", "oficial_lan": None, "tz": "Europe/Helsinki"},
+            {"iso_code": "FR", "name": "France", "oficial_lan": "French", "tz": "Europe/Paris"},
+            {"iso_code": "GR", "name": "Greece", "oficial_lan": "Greek", "tz": "Europe/Athens"},
+            {"iso_code": "HU", "name": "Hungary", "oficial_lan": None, "tz": "Europe/Budapest"},
+            {"iso_code": "IS", "name": "Iceland", "oficial_lan": "Icelandic", "tz": "Atlantic/Reykjavik"},
+            {"iso_code": "IT", "name": "Italy", "oficial_lan": "Italian", "web_lan": "Italian", "tz": "Europe/Rome"},
+            {"iso_code": "IE", "name": "Ireland", "oficial_lan": "English", "tz": "Europe/Dublin"},
+            {"iso_code": "LV", "name": "Latvia", "oficial_lan": None, "tz": "Europe/Riga"},
+            {"iso_code": "LT", "name": "Lithuania", "oficial_lan": None, "tz": "Europe/Vilnius"},
+            {"iso_code": "LU", "name": "Luxembourg", "oficial_lan": "French", "tz": "Europe/Luxembourg"},
+            {"iso_code": "MK", "name": "Macedonia", "oficial_lan": None, "tz": "Europe/Skopje"},
+            {"iso_code": "MT", "name": "Malta", "oficial_lan": "English", "tz": "Europe/Malta"},
+            {"iso_code": "ME", "name": "Montenegro", "oficial_lan": None, "tz": "Europe/Podgorica"},
+            {"iso_code": "MD", "name": "Moldova", "oficial_lan": None, "tz": "Europe/Chisinau"},
+            {"iso_code": "NL", "name": "Netherlands", "oficial_lan": None, "tz": "Europe/Amsterdam"},
+            {"iso_code": "NO", "name": "Norway", "oficial_lan": None, "tz": "Europe/Oslo"},
+            {"iso_code": "PL", "name": "Poland", "oficial_lan": None, "web_lan": "Polish", "tz": "Europe/Warsaw"},
+            {"iso_code": "PT", "name": "Portugal", "oficial_lan": "Portuguese", "web_lan": "Portuguese", "tz": "Europe/Lisbon"},
+            {"iso_code": "RO", "name": "Romania", "oficial_lan": None, "tz": "Europe/Bucharest"},
+            {"iso_code": "RS", "name": "Serbia", "oficial_lan": None, "tz": "Europe/Belgrade"},
+            {"iso_code": "RU", "name": "Russia", "oficial_lan": None, "tz": "Europe/Moscow"},
+            {"iso_code": "SK", "name": "Slovakia", "oficial_lan": None, "tz": "Europe/Bratislava"},
+            {"iso_code": "SI", "name": "Slovenia", "oficial_lan": None, "tz": "Europe/Ljubljana"},
+            {"iso_code": "ES", "name": "Spain", "oficial_lan": "Spanish", "tz": "Europe/Madrid"},
+            {"iso_code": "SE", "name": "Sweden", "oficial_lan": "Swedish", "tz": "Europe/Stockholm"},
+            {"iso_code": "GB", "name": "United Kingdom", "oficial_lan": "English", "tz": "Europe/London"},
+            {"iso_code": "GI", "name": "Gibraltar", "oficial_lan": "English", "tz": "Europe/Gibraltar"}, #include bus station in La Línea de la Concepción
+            {"iso_code": "AD", "name": "Andorra", "oficial_lan": None, "tz": "Europe/Andorra"},
+            # {"iso_code": "MC", "name": "Monaco", "oficial_lan": None, "tz": "Europe/Monaco"}, #no bus, train or car-pooling to be found in APIs
+            # {"iso_code": "SM", "name": "San Marino", "oficial_lan": None, "web_lan": "Italian", "tz": "Europe/Rome"}, #no bus, train or car-pooling to be found in APIs
+            {"iso_code": "LI", "name": "Liechtenstein", "oficial_lan": None, "tz": "Europe/Paris"},
+            {"iso_code": "UA", "name": "Ukraine", "oficial_lan": None, "tz": "Europe/Kiev"},
+            {"iso_code": "BY", "name": "Belarus", "oficial_lan": None, "tz": "Europe/Minsk"},
+            {"iso_code": "XK", "name": "Kosovo", "oficial_lan": None, "tz": "Europe/Belgrade"}, #clear out_of_scope at traveilnho.py
+            {"iso_code": "TR", "name": "Turkey", "oficial_lan": "Turkish", "tz": "Europe/Istanbul"},
+        ]
